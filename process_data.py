e748f884ea4fc2c6bc23a5c0a7d4dd79c10901a3
ChrisBly <chrisbnsw@gmail.com>
2019-10-20 12:12:03 +1100

    Add files via upload

---
+++ process_data.py
@@ -0,0 +1,88 @@
+import sys
+# import libraries
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+# ETL Pipeline Preparation
+
+def load_data(messages_filepath, categories_filepath):
+    # 1. load datasets.
+    # input paths:
+    # messages_filepath
+    # categories_filepath
+    # Load messages.csv into a dataframe 
+    messages = pd.read_csv(messages_filepath)
+    # Load categories.csv into a dataframe 
+    categories = pd.read_csv(categories_filepath)
+    # Merge Data set
+    df = messages.merge(categories, how = 'left', on = ['id'])
+    return df
+
+
+def clean_data(df):
+    # Section 2 Clean:
+    # Input Dataframe: df from load_data
+    # Split categories into separate category columns
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand = True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0,:]
+    # use this row to extract a list of new column names for categories.
+    category_colnames = list(map(lambda i: i[ : -2], row)) 
+    categories.columns = category_colnames
+    # Convert category values to just numbers 0 or 1.
+    for column in categories:
+    # set each value to be the last character of the string
+        categories[column] = categories[column].transform(lambda x: x[-1:])
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+        categories[column] = categories[column].astype('int')
+        # Replace categories column in df with new category columns.
+    df.drop(['categories'],axis=1,inplace=True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    # replacing 2 value with 1 value
+    df.replace(2, 1, inplace=True)
+    # Remove duplicates
+    df.drop_duplicates(inplace=True)
+    return df
+
+
+def save_data(df, database_filename):
+    # input files:
+    # df - data frame
+    # database_filename
+    # Save the clean dataset into an sqlite database  
+    name = 'sqlite:///' + database_filename
+    engine = create_engine(name)
+    df.to_sql('Disasters', engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ train_classifier.py
@@ -0,0 +1,165 @@
+import sys
+# import libraries
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+from sqlalchemy import create_engine
+import re 
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from nltk.corpus import stopwords 
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+def load_data(database_filepath):
+    """
+    Doc String: 
+    Inputs:
+    
+    Input Files 1:
+    Loading Database from ETL pipeline.
+    
+    Returns: ETL pipeline Database 
+        
+    """
+       
+    # Combining the sqlite:/// + database_filepath
+    engine = create_engine('sqlite:///' + database_filepath)
+    # Creating a Dataframefrom the SQL Table
+    df = pd.read_sql_table('Disasters', engine)
+    # Define the Inputs for the Disaster Recovery data
+    X = df['message'] # Define feature 
+    # Create category_names
+    Y = pd.concat([df.iloc[:,4:40],df.iloc[:,-1:]],axis = 1)
+    category_names = Y.columns
+    
+    return X,Y,category_names
+
+
+def tokenize(text):
+    """
+    Doc String: 
+    Inputs:
+    
+    Text Data gets tokenize text , normalize, lemmatize, 
+    using the nltk package
+    
+    Returns: Text data that has been normalize, lemmatize, and tokenize 
+        
+    """
+    # Normalize
+    # Set text to lower case and remove punctuation
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text) # Remove punctuation characters
+    # Tokenize words 
+    tokens = word_tokenize(text)
+    # lemmatizer and remove stopwords
+    # lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    # stopwords
+    stop_words = set(stopwords.words('english'))
+    # lemmatizer and remove stopwords
+    output = [lemmatizer.lemmatize(w) for w in tokens if not w in stop_words]
+    output = [] 
+    for w in tokens: 
+        if w not in stop_words: 
+            output.append(w)
+    return output
+
+def build_model():
+    '''
+    Doc String:
+    Build Model using GridSearchCV/pipeline
+    
+    Looking at the default hyper parameters:
+    the n_estimators is set to 'warn' default
+    min_samples_split is set to 2.
+      
+    Changing default hyper parameters to improve performanace.
+    '''
+    # Create pipeline with Classifier
+    pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer = tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultiOutputClassifier(RandomForestClassifier()))])
+    # Changing default hyper parameters to improve performanace.
+    parameters = {'clf__estimator__n_estimators':[10,20],
+              'clf__estimator__max_depth':[5,15],
+             'clf__estimator__min_samples_split':[5,10]}
+    # grid search
+    model = GridSearchCV(pipeline, parameters)
+    return model
+
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    '''
+    Doc String
+    
+    Test your model using sklearn's  classification_report
+    Report the f1 score, precision and recall for each output category of the dataset. 
+    
+    '''
+    Y_pred = model.predict(X_test)
+    Y_Predicton_df = pd.DataFrame(Y_pred, columns = Y_test.columns)
+    # iterating through the columns 
+    for column in Y_test:
+        for i in Y_test.columns:
+            print('Catergory: {}\n'.format(column))
+            print(classification_report(Y_test[column],Y_Predicton_df[column])) #calling sklearn's classification_report on each
+   
+
+   
+  
+
+
+def save_model(model, model_filepath):
+    '''
+    Doc String:
+    Saving Model
+    '''
+    pickle.dump(model, open('model.pkl', 'wb'))
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
